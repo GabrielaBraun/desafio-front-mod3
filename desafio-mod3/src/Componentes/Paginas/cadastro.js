c08b683b201748b53b6bca8deba289c259f5ca86
unknown <gabitelhado@gmail.com>
2021-08-08 10:56:57 -0300

    adionando icone de visibilidade da senha

--- desafio-mod3/src/Componentes/Paginas/cadastro.js
+++ desafio-mod3/src/Componentes/Paginas/cadastro.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import {Card, TextField, Typography, Button} from '@material-ui/core';
+import {Card, TextField, Typography, Button, InputAdornment, IconButton, Input, InputLabel, FormControl} from '@material-ui/core';
+import {VisibilityOff} from '@material-ui/icons';
 import './styles.css';
 import useStyles from './style';
 
@@ -14,8 +15,38 @@ function Cadastro(){
                     <TextField label="Seu Nome" />
                     <TextField label="Nome da loja" />
                     <TextField label="E-mail" />
-                    <TextField label="Senha" />
-                    <TextField label="Repita a senha" />
+                    <FormControl>
+                        <InputLabel htmlFor="senha">Senha</InputLabel>
+                        <Input 
+                            id="senha"
+                            type="password"
+                            endAdornment={
+                                <InputAdornment position="end">
+                                <IconButton
+                                    aria-label="toggle password visibility"
+                                >
+                                    <VisibilityOff />
+                                    </IconButton>
+                                </InputAdornment>
+                            } 
+                        />
+                    </FormControl>
+                    <FormControl>
+                        <InputLabel htmlFor="senharepetida">Repita a Senha</InputLabel>
+                        <Input 
+                            id="senharepetida"
+                            type="password"
+                            endAdornment={
+                                <InputAdornment position="end">
+                                <IconButton
+                                    aria-label="toggle password visibility"
+                                >
+                                    <VisibilityOff />
+                                    </IconButton>
+                                </InputAdornment>
+                            } 
+                        />
+                    </FormControl>
                 </div>
                 <Button className={classes.button} variant="contained" color="primary">Criar conta</Button>
                 <Typography variant="p" component="p">Já possui cadastro? ACESSE</Typography>
