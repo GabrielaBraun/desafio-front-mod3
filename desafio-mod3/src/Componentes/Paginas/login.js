434738c8b545809891140aaa45b76873634a1fc5
unknown <gabitelhado@gmail.com>
2021-08-09 09:07:46 -0300

    tentando formulário login

--- desafio-mod3/src/Componentes/Paginas/login.js
+++ desafio-mod3/src/Componentes/Paginas/login.js
@@ -3,19 +3,50 @@ import {Button, Card, TextField, Typography} from '@material-ui/core';
 import './styles.css';
 import useStyles from './style';
 import InputSenha from '../InputSenha';
+import { useForm } from 'react-hook-form';
+import { useHistory } from 'react-router-dom';
 
 function Login(){
     const classes = useStyles();
+    const history = useHistory();
+    const { handleSubmit, register, formState: {errors} } = useForm();
+
+    async function logar(data){
+
+        const resposta = await fetch('http://localhost:3000/login', {
+            method: "POST",
+            body: JSON.stringify(data),
+            headers: {
+                'Content-type': 'application/json'
+            }
+        });
+
+        if(resposta.ok){
+            return history.push('/produtos');
+        }
+
+        console.log("Erro");
+    };
+
     return(
-        <div className="container">
+        <div className="container" onSubmit={handleSubmit(logar)}>
             <Card className={classes.card}>
                 <Typography variant="h4" component="h2">Login</Typography>
                     <div className={classes.columns}>
-                        <TextField label="E-mail" />
-                        <InputSenha label="Senha" id="senha" />           
+                        <TextField 
+                            label="E-mail"
+                            error={!!errors.email} 
+                            {...register('email', {required: true})}
+                        />
+                        <InputSenha 
+                            label="Senha" 
+                            id="senha" 
+                            error={!!errors.senha}
+                            register={() => register('senha', {required: true})}
+                        />   
                     </div>
-                <Button className={classes.button} variant="contained" color="primary">Entrar</Button>
-                <Typography variant="p" component="p">Primeira vez aqui? CRIE UMA CONTA</Typography>
+                <Button type="submit" className={classes.button} variant="contained" color="primary">Entrar</Button>
+                <Typography variant="h6" component="p">Primeira vez aqui? <a href="/cadastro"> CRIE UMA CONTA</a></Typography>
             </Card>
         </div>
     )
